1a0251e83a824896063427d9d724c4e1647979bc
Felipe Emerson <felipeemersonsb@hotmail.com>
2017-12-14 12:54:45 -0300

    adiciona sistema de login

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -2,6 +2,8 @@ app.controller("hearMeController", function($scope){
 	$scope.artistas = [];
 	$scope.playlists = [];
 	$scope.musicasNoSistema = [];
+	$scope.usuarios = [];
+	$scope.usuarioLogado = null;
 
 	$scope.novoArtista = {};
 
@@ -15,7 +17,7 @@ app.controller("hearMeController", function($scope){
 			artista.nota = 'N/A';
 			artista.ultimaMusica = 'N/A';
 			artista.favorito = false;
-			$scope.artistas.push(artista);
+			$scope.usuarioLogado.artistas.push(artista);
 			$scope.novoArtista = {};
 			alert(artista.nome + ' adicionado(a)!');
 		}else {
@@ -24,8 +26,8 @@ app.controller("hearMeController", function($scope){
 	}
 
 	$scope.artistaExiste = function(nome){ 
-		for(var indiceArtista = 0; indiceArtista < $scope.artistas.length; indiceArtista++){
-			if(nome == $scope.artistas[indiceArtista].nome){
+		for(var indiceArtista = 0; indiceArtista < $scope.usuarioLogado.artistas.length; indiceArtista++){
+			if(nome == $scope.usuarioLogado.artistas[indiceArtista].nome){
 				return true;
 			}
 		}
@@ -58,18 +60,18 @@ app.controller("hearMeController", function($scope){
 			alert('Música já existe!');
 		} else if($scope.albumExiste){
 			$scope.albumExistente.musicas.push(musica);
-			$scope.musicasNoSistema.push({nome: $scope.novaMusica.nome, artista: $scope.novaMusica.artista});
+			$scope.usuarioLogado.musicasNoSistema.push({nome: $scope.novaMusica.nome, artista: $scope.novaMusica.artista});
 			alert('Música adicionada no álbum: ' + $scope.albumExistente.nome);
 		} else {
 			var novoAlbum = {};
 			novoAlbum.nome = $scope.novaMusica.album;
 			novoAlbum.musicas = [];
 			novoAlbum.musicas.push(musica);
-			$scope.musicasNoSistema.push({nome: $scope.novaMusica.nome, artista: $scope.novaMusica.artista});
+			$scope.usuarioLogado.musicasNoSistema.push({nome: $scope.novaMusica.nome, artista: $scope.novaMusica.artista});
 
-			for(var indiceArtista = 0; indiceArtista < $scope.artistas.length; indiceArtista++){
-				if($scope.novaMusica.artista == $scope.artistas[indiceArtista].nome){
-					$scope.artistas[indiceArtista].albuns.push(novoAlbum);
+			for(var indiceArtista = 0; indiceArtista < $scope.usuarioLogado.artistas.length; indiceArtista++){
+				if($scope.novaMusica.artista == $scope.usuarioLogado.artistas[indiceArtista].nome){
+					$scope.usuarioLogado.artistas[indiceArtista].albuns.push(novoAlbum);
 					alert('Música adicionada no álbum: ' + novoAlbum.nome);
 				}
 			}
@@ -82,16 +84,16 @@ app.controller("hearMeController", function($scope){
 	}
 
 	$scope.validaMusica = function() {
-		for(var indiceArtista = 0; indiceArtista < $scope.artistas.length; indiceArtista++){
+		for(var indiceArtista = 0; indiceArtista < $scope.usuarioLogado.artistas.length; indiceArtista++){
 
-			for(var indiceAlbum = 0; indiceAlbum < $scope.artistas[indiceArtista].albuns.length; indiceAlbum++){
+			for(var indiceAlbum = 0; indiceAlbum < $scope.usuarioLogado.artistas[indiceArtista].albuns.length; indiceAlbum++){
 
-				if($scope.novaMusica.artista == $scope.artistas[indiceArtista].nome && $scope.novaMusica.album == $scope.artistas[indiceArtista].albuns[indiceAlbum].nome){
+				if($scope.novaMusica.artista == $scope.usuarioLogado.artistas[indiceArtista].nome && $scope.novaMusica.album == $scope.usuarioLogado.artistas[indiceArtista].albuns[indiceAlbum].nome){
 					$scope.albumExiste = true;
-					$scope.albumExistente = $scope.artistas[indiceArtista].albuns[indiceAlbum];
+					$scope.albumExistente = $scope.usuarioLogado.artistas[indiceArtista].albuns[indiceAlbum];
 
-					for(var indiceMusica = 0; indiceMusica < $scope.artistas[indiceArtista].albuns[indiceAlbum].musicas.length; indiceMusica++){
-						if($scope.novaMusica.nome == $scope.artistas[indiceArtista].albuns[indiceAlbum].musicas[indiceMusica].nome){
+					for(var indiceMusica = 0; indiceMusica < $scope.usuarioLogado.artistas[indiceArtista].albuns[indiceAlbum].musicas.length; indiceMusica++){
+						if($scope.novaMusica.nome == $scope.usuarioLogado.artistas[indiceArtista].albuns[indiceAlbum].musicas[indiceMusica].nome){
 							$scope.musicaExiste = true;
 							return;
 						}
@@ -145,7 +147,7 @@ app.controller("hearMeController", function($scope){
 			playlist.nome = $scope.novaPlaylist.nome;
 			playlist.id = Date.now();
 			playlist.musicas = [];
-			$scope.playlists.push(playlist);
+			$scope.usuarioLogado.playlists.push(playlist);
 		}
 		
 		$scope.novaPlaylist = {};
@@ -153,17 +155,17 @@ app.controller("hearMeController", function($scope){
 
 	$scope.removePlaylist = function(playlist){
 		if(confirm("Excluir a playlist " + playlist.nome + "?")){
-			for(var indicePlaylist = 0; indicePlaylist < $scope.playlists.length; indicePlaylist++){
-				if($scope.playlists[indicePlaylist].nome == playlist.nome){
-					$scope.playlists.splice(indicePlaylist, 1);
+			for(var indicePlaylist = 0; indicePlaylist < $scope.usuarioLogado.playlists.length; indicePlaylist++){
+				if($scope.usuarioLogado.playlists[indicePlaylist].nome == playlist.nome){
+					$scope.usuarioLogado.playlists.splice(indicePlaylist, 1);
 				}
 			}
 		}	
 	}
 
 	$scope.verificaNomePlaylist = function() {
-		for(indicePlaylist = 0; indicePlaylist < $scope.playlists.length; indicePlaylist++){
-			if($scope.playlists[indicePlaylist].nome == $scope.novaPlaylist.nome){
+		for(indicePlaylist = 0; indicePlaylist < $scope.usuarioLogado.playlists.length; indicePlaylist++){
+			if($scope.usuarioLogado.playlists[indicePlaylist].nome == $scope.novaPlaylist.nome){
 				return false;
 			}
 		}
@@ -174,7 +176,7 @@ app.controller("hearMeController", function($scope){
 
 	$scope.adicionaMusicaNaPlaylist = function(playlist) {
 		if($scope.novaMusicaNaPlaylist.nome == "-- Selecione uma música --" || $scope.novaMusicaNaPlaylist.nome == undefined){
-			if($scope.musicasNoSistema.length == 0){
+			if($scope.usuarioLogado.musicasNoSistema.length == 0){
 				alert('Nenhuma música cadastrada no sistema! Adicione músicas!');
 				return;
 			} else {
@@ -214,6 +216,83 @@ app.controller("hearMeController", function($scope){
 				}				
 			}
 		}
+	}
+
+	$scope.novoUsuario = {};
+
+	$scope.registrarUsuario = function() {
+		if($scope.senhasDiferentes()){
+			alert('As senhas são diferentes! Tente novamente!');
+		} else {
+			if($scope.usuarioValido()){
+				var usuario = {};
+				usuario.nome = $scope.novoUsuario.nome;
+				usuario.email = $scope.novoUsuario.email;
+				usuario.senha = $scope.novoUsuario.senha;
+				usuario.artistas = [];
+				usuario.playlists = [];
+				usuario.musicasNoSistema = [];
+				$scope.usuarios.push(usuario);
+				alert('Usuário: ' + $scope.novoUsuario.nome + ' adicionado!');
+			} else {
+				alert('Usuário já existe!');
+			}
+		}
+
+		$scope.novoUsuario = {};
+	}
+
+	$scope.usuarioValido = function(){
+		for(var indiceUsuario = 0; indiceUsuario < $scope.usuarios.length; indiceUsuario++){
+			if($scope.usuarioExiste($scope.usuarios[indiceUsuario])){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	$scope.usuarioExiste = function(usuarioAtual){
+		if($scope.novoUsuario.email == usuarioAtual.email){
+			return true;
+		}
+
+		return false;
+	}
+
+	$scope.senhasDiferentes = function(){
+		return $scope.novoUsuario.senha != $scope.novoUsuario.senhaRepetida;
+	}
+
+	$scope.novoUsuarioLogin = {};
+
+	$scope.fazerLogin = function(){
+		if($scope.emailNaoExiste()){
+			alert('Usuário não registrado!');
+		} else {
+			for(var indiceUsuario = 0; indiceUsuario < $scope.usuarios.length; indiceUsuario++){
+				if($scope.novoUsuarioLogin.emailLogin == $scope.usuarios[indiceUsuario].email){
+					if($scope.novoUsuarioLogin.senhaLogin == $scope.usuarios[indiceUsuario].senha){
+						$scope.usuarioLogado = $scope.usuarios[indiceUsuario];
+						alert('Usuário: ' + $scope.usuarioLogado.nome + ' logado!');
+					} else {
+						alert('Senha errada!');
+					}
+				}
+			}
+		}
+		
+		$scope.novoUsuarioLogin = {};
+	}
+
+	$scope.emailNaoExiste = function(){
+		for(var indiceUsuario = 0; indiceUsuario < $scope.usuarios.length; indiceUsuario++){
+			if($scope.novoUsuarioLogin.emailLogin == $scope.usuarios[indiceUsuario].email){
+				return false;
+			}
+		}
+
+		return true;
 	}
 	
 });	
