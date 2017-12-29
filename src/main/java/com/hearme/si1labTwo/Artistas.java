3720d8afd0736d80f68e0581a5187c9ffe7dfc4e
Felipe Emerson <felipeemersonsb@hotmail.com>
2017-12-29 16:44:06 -0300

    final commit por enquanto
    é só isso! nao tem mais jeito. acabou...

--- src/main/java/com/hearme/si1labTwo/Artistas.java
+++
@@ -1,7 +0,0 @@
-package com.hearme.si1labTwo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Artistas extends JpaRepository<Artista, Long> {
-
-}
--- src/main/java/com/hearme/si1labTwo/Playlists.java
+++
@@ -1,7 +0,0 @@
-package com.hearme.si1labTwo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Playlists extends JpaRepository<Playlist, Long>{
-
-}
